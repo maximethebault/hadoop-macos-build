dcf2f00d0643f8364f98934bf22ac0f5ae071a5b
Maxime <mthebault@advalo.com>
2022-10-31 20:03:19 +0100

    Build test

--- .github/workflows/build-hadoop.sh
+++ .github/workflows/build-hadoop.sh
@@ -8,12 +8,14 @@ export PKG_CONFIG_PATH="${OPENSSL_ROOT_DIR}/lib/pkgconfig"
 curl -L https://github.com/apache/hadoop/archive/refs/tags/rel/release-$HADOOP_VERSION.tar.gz | gunzip | tar -x \
  && mv hadoop-rel-release-$HADOOP_VERSION hb \
  && cd hb \
- && sed -i. '1s/^/cmake_policy(SET CMP0074 NEW) /' hadoop-common-project/hadoop-common/src/CMakeLists.txt \
+ && sed -i '' -e "24s/^//p; 24s/^.*/cmake_policy(SET CMP0074 NEW)/" hadoop-common-project/hadoop-common/src/CMakeLists.txt \
+ && cat hadoop-common-project/hadoop-common/src/CMakeLists.txt \
  && export CXXFLAGS="-std=c++14" \
  && mvn package -Pdist,native -Drequire.isal -Drequire.pmdk -DskipTests -Dmaven.javadoc.skip=true -Dtar --no-transfer-progress \
- && tar xf /tmp/hb/hadoop-dist/target/hadoop-$HADOOP_VERSION.tar.gz -C /opt \
- && rm -fr /opt/hadoop-$HADOOP_VERSION/lib/native/examples \
- && ls -l  /opt/hadoop-$HADOOP_VERSION/lib/native/ \
- && sh -c '/opt/hadoop-$HADOOP_VERSION/bin/hadoop checknative -a || true' \
+ && cd .. \
+ && tar xf hb/hadoop-dist/target/hadoop-$HADOOP_VERSION.tar.gz -C . \
+ && rm -fr hadoop-$HADOOP_VERSION/lib/native/examples \
+ && ls -l  hadoop-$HADOOP_VERSION/lib/native/ \
+ && sh -c 'hadoop-$HADOOP_VERSION/bin/hadoop checknative -a || true'
 
 
