20c0d83e53473357efd6bca929e16f819e6cf8e4
Maxime <mthebault@advalo.com>
2022-10-31 19:06:25 +0100

    Build test

--- .github/workflows/install-deps.sh
+++ .github/workflows/install-deps.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-brew install gcc cmake autoconf automake libtool protobuf snappy maven zstd cyrus-sasl isa-l gzip bzip2 zlib openssl@1.1
-brew remove protobuf
+brew install gcc cmake autoconf automake libtool snappy maven zstd cyrus-sasl isa-l gzip bzip2 zlib openssl@1.1
