38697de15a2e60479af49824757130302254b148
Maxime <mthebault@advalo.com>
2022-10-31 19:51:53 +0100

    Build test

--- .github/workflows/build-hadoop.sh
+++ .github/workflows/build-hadoop.sh
@@ -8,7 +8,7 @@ export PKG_CONFIG_PATH="${OPENSSL_ROOT_DIR}/lib/pkgconfig"
 curl -L https://github.com/apache/hadoop/archive/refs/tags/rel/release-$HADOOP_VERSION.tar.gz | gunzip | tar -x \
  && mv hadoop-rel-release-$HADOOP_VERSION hb \
  && cd hb \
- && echo "cmake_policy(SET CMP0074 NEW)" >> hadoop-common-project/hadoop-common/src/CMakeLists.txt \
+ && sed -i. '1s/^/cmake_policy(SET CMP0074 NEW) /' hadoop-common-project/hadoop-common/src/CMakeLists.txt \
  && export CXXFLAGS="-std=c++14" \
  && mvn package -Pdist,native -Drequire.isal -Drequire.pmdk -DskipTests -Dmaven.javadoc.skip=true -Dtar --no-transfer-progress \
  && tar xf /tmp/hb/hadoop-dist/target/hadoop-$HADOOP_VERSION.tar.gz -C /opt \
