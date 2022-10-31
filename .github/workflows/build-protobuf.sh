e7d4a9a08cfe5a2a08d1d98c8992a5d6336bc36e
Maxime <mthebault@advalo.com>
2022-10-31 18:53:39 +0100

    Build test

--- .github/workflows/build-protobuf.sh
+++ .github/workflows/build-protobuf.sh
@@ -3,6 +3,7 @@
 export PROTOBUF_VERSION=3.7.1
 
 curl -L https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOBUF_VERSION/protobuf-java-$PROTOBUF_VERSION.tar.gz | gunzip | tar -x \
+ && mv protobuf-$PROTOBUF_VERSION protobuf \
  && cd protobuf \
  && ./configure --prefix=/usr/local/Cellar/protobuf/$PROTOBUF_VERSION \
  && make -j$(nproc) \
