da13d38c3111ca685616e9f6d0b5c66f21b972cd
Maxime <mthebault@advalo.com>
2022-10-31 17:35:37 +0100

    Build test

--- .github/workflows/build-hadoop.sh
+++ .github/workflows/build-hadoop.sh
@@ -4,5 +4,34 @@ brew install cmake
 brew install zlib
 brew install protobuf
 brew install snappy
-brew install homebrew/versions/protobuf250
-echo 'export PATH="/usr/local/opt/protobuf@2.5/bin:$PATH"' >> ~/.bashrc
+brew install maven
+brew install zstd
+brew install cyrus-sasl
+brew install isa-l
+brew install bzip2
+brew install openssl@1.1
+
+export PROTOBUF_VERSION=3.7.1
+
+curl -L https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOBUF_VERSION/protobuf-java-$PROTOBUF_VERSION.tar.gz | gunzip | tar -x \
+ && cd protobuf-$PROTOBUF_VERSION \
+ && ./configure \
+ && make -j$(nproc) \
+ && make install \
+ && cd .. \
+ && rm -fr protobuf-* \
+ && ldconfig /usr/local/lib
+
+export HADOOP_VERSION=3.3.4
+
+curl -L https://github.com/apache/hadoop/archive/refs/tags/rel/release-$HADOOP_VERSION.tar.gz | gunzip | tar -x \
+ && mv hadoop-rel-release-$HADOOP_VERSION hb \
+ && cd hb \
+ && export CXXFLAGS="-std=c++14" \
+ && mvn package -Pdist,native -Drequire.isal -Drequire.pmdk -DskipTests -Dmaven.javadoc.skip=true -Dtar --no-transfer-progress \
+ && tar xf /tmp/hb/hadoop-dist/target/hadoop-$HADOOP_VERSION.tar.gz -C /opt \
+ && rm -fr /opt/hadoop-$HADOOP_VERSION/lib/native/examples \
+ && ls -l  /opt/hadoop-$HADOOP_VERSION/lib/native/ \
+ && sh -c '/opt/hadoop-$HADOOP_VERSION/bin/hadoop checknative -a || true' \
+
+
