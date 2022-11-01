fe69d6f371321c94222f72011b1f1de74eb51ffa
Maxime <mthebault@advalo.com>
2022-11-01 23:59:11 +0100

    Build test

--- .github/workflows/install-deps.sh
+++ .github/workflows/install-deps.sh
@@ -4,3 +4,6 @@ set -e
 
 # don't install ISA-L as it's causing issues during compile
 brew install gcc@10 cmake autoconf automake libtool snappy maven zstd cyrus-sasl gzip bzip2 zlib openssl@1.1
+
+brew link zstd
+brew link snappy
